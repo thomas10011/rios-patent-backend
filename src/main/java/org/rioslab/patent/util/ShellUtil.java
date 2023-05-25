08d591e41ef75785b8cf5fc30dfb36198b774c44
thomas10011 <tanhuiri@gmail.com>
2023-05-25 13:53:01 +0800

    fix: write pom.xml when create new template

--- src/main/java/org/rioslab/patent/util/ShellUtil.java
+++ src/main/java/org/rioslab/patent/util/ShellUtil.java
@@ -28,6 +28,103 @@ public class ShellUtil {
         "}"
         ;
 
+    public static String pomCode = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" +
+            "         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+            "         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\">\n" +
+            "    <modelVersion>4.0.0</modelVersion>\n" +
+            "\n" +
+            "    <groupId>riolab.org</groupId>\n" +
+            "    <artifactId>rios-patent-execute</artifactId>\n" +
+            "    <version>1.0-SNAPSHOT</version>\n" +
+            "\n" +
+            "    <properties>\n" +
+            "        <spark.version>3.2.4</spark.version>\n" +
+            "        <jedis.version>4.3.0</jedis.version>\n" +
+            "\n" +
+            "        <maven.compiler.source>8</maven.compiler.source>\n" +
+            "        <maven.compiler.target>8</maven.compiler.target>\n" +
+            "        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>\n" +
+            "    </properties>\n" +
+            "\n" +
+            "\n" +
+            "    <dependencies>\n" +
+            "        <dependency>\n" +
+            "            <groupId>org.apache.spark</groupId>\n" +
+            "            <artifactId>spark-core_2.13</artifactId>\n" +
+            "            <version>${spark.version}</version>\n" +
+            "        </dependency>\n" +
+            "        <dependency>\n" +
+            "            <groupId>org.apache.spark</groupId>\n" +
+            "            <artifactId>spark-sql_2.13</artifactId>\n" +
+            "            <version>${spark.version}</version>\n" +
+            "        </dependency>\n" +
+            "        <!-- Redis 连接 -->\n" +
+            "        <dependency>\n" +
+            "            <groupId>redis.clients</groupId>\n" +
+            "            <artifactId>jedis</artifactId>\n" +
+            "            <version>${jedis.version}</version>\n" +
+            "        </dependency>\n" +
+            "    </dependencies>\n" +
+            "\n" +
+            "\n" +
+            "    <build>\n" +
+            "        <plugins>\n" +
+            "            <!-- A plugin for compiling Java code -->\n" +
+            "            <plugin>\n" +
+            "                <groupId>org.apache.maven.plugins</groupId>\n" +
+            "                <artifactId>maven-compiler-plugin</artifactId>\n" +
+            "                <version>3.8.1</version>\n" +
+            "                <configuration>\n" +
+            "                    <source>1.8</source>\n" +
+            "                    <target>1.8</target>\n" +
+            "                </configuration>\n" +
+            "            </plugin>\n" +
+            "            <!-- Or, a plugin for compiling Scala code -->\n" +
+            "            <!-- Make sure you are not using \"maven-scala-plugin\", which is the older version -->\n" +
+            "            <plugin>\n" +
+            "                <groupId>net.alchim31.maven</groupId>\n" +
+            "                <artifactId>scala-maven-plugin</artifactId>\n" +
+            "                <version>4.2.4</version>\n" +
+            "                <executions>\n" +
+            "                    <execution>\n" +
+            "                        <goals>\n" +
+            "                            <goal>compile</goal>\n" +
+            "                        </goals>\n" +
+            "                    </execution>\n" +
+            "                </executions>\n" +
+            "            </plugin>\n" +
+            "            <!-- A plugin for creating an assembly JAR file (Java or Scala) -->\n" +
+            "            <plugin>\n" +
+            "                <groupId>org.apache.maven.plugins</groupId>\n" +
+            "                <artifactId>maven-shade-plugin</artifactId>\n" +
+            "                <version>3.2.1</version>\n" +
+            "                <executions>\n" +
+            "                    <execution>\n" +
+            "                        <phase>package</phase>\n" +
+            "                        <goals>\n" +
+            "                            <goal>shade</goal>\n" +
+            "                        </goals>\n" +
+            "                        <configuration>\n" +
+            "                            <filters>\n" +
+            "                                <filter>\n" +
+            "                                    <artifact>*:*</artifact>\n" +
+            "                                    <excludes>\n" +
+            "                                        <exclude>META-INF/*.SF</exclude>\n" +
+            "                                        <exclude>META-INF/*.DSA</exclude>\n" +
+            "                                        <exclude>META-INF/*.RSA</exclude>\n" +
+            "                                    </excludes>\n" +
+            "                                </filter>\n" +
+            "                            </filters>\n" +
+            "                        </configuration>\n" +
+            "                    </execution>\n" +
+            "                </executions>\n" +
+            "            </plugin>\n" +
+            "        </plugins>\n" +
+            "    </build>\n" +
+            "</project>"
+            ;
+
     public static ExecDTO pack(String packageName, String className, String code, String codeID) {
         // 如果其中一个为空，执行默认的程序
         ExecDTO res = new ExecDTO();
@@ -92,9 +189,11 @@ public class ShellUtil {
 
     private static boolean writeMain(String packageName, String className, String codeID) {
         String mainDir = "/tmp/patent/"+ codeID + "/rios-patent-execute/src/main/java/";
+        String pomDir = "/tmp/patent/"+ codeID + "/rios-patent-execute/";
         String code = mainCode.replace("PACKAGE", packageName + "." + className).replace("APPLICATION", className);
+
         // 写入代码
-        return writeFile(mainDir, className + ".scala", code);
+        return writeFile(mainDir, className + ".scala", code) && writeFile(pomDir, "pom.xml", pomCode);
     }
 
     private static boolean writeCode(String packageName, String className, String code, String codeID) {
